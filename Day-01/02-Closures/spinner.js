71e75a63a7f5b3479d11d747c02e3b750c110677
tkmagesh <tkmagesh77@gmail.com>
2015-03-23 15:05:33 +0530

    Closures - Assignment

---
+++ Day-01/02-Closures/spinner.js
@@ -0,0 +1,17 @@
+//create an object "spinner"
+
+var spinner = //.....
+
+//"spinner" should exhibit the following behavior
+
+spinner.up() => 1
+spinner.up() => 2
+spinner.up() => 3
+spinner.up() => 4
+
+spinner.down() => 3
+spinner.down() => 2
+spinner.down() => 1
+spinner.down() => 0
+spinner.down() => -1
+
