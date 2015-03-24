e0d7e9bb7fc8e97d78e7da4a41cacc7ee6025c1b
tkmagesh <tkmagesh77@gmail.com>
2015-03-24 12:05:22 +0530

    Functional Programming- With Sort

--- Day-02/03-FunctionalProgramming/products.js
+++ Day-02/03-FunctionalProgramming/products.js
@@ -18,6 +18,84 @@ print("Default List", function(){
 });
 
 print("Sort", function(){
-    console.table(products);
+    print("Default [products by id]", function(){
+        function sort(){
+            for(var i=0; i<products.length-1; i++)
+                for(var j = i+1; j<products.length; j++){
+                    var left = products[i],
+                        right = products[j];
+                    if (left.id > right.id){
+                        products[i] = products[j];
+                        products[j] = left;
+                    }
+                }
+        }
+        sort();
+        console.table(products);
+    });
+    print("Generic Sort [Any list by any attribute]", function(){
+        function sort(list, attrName){
+            for(var i=0; i<list.length-1; i++)
+                for(var j = i+1; j<list.length; j++){
+                    var left = list[i],
+                        right = list[j];
+                    if (left[attrName] > right[attrName]){
+                        list[i] = list[j];
+                        list[j] = left;
+                    }
+                }
+        }
+        print("Products sorted by name", function(){
+            sort(products, "name");
+            console.table(products);
+        });
+        print("Products sorted by cost", function(){
+            sort(products, "cost");
+            console.table(products);
+        });
+    });
+    print("Generic Sort [Any list by any logic]", function(){
+        function sort(list, comparerFn){
+            for(var i=0; i<list.length-1; i++)
+                for(var j = i+1; j<list.length; j++){
+                    var left = list[i],
+                        right = list[j];
+                    if (comparerFn(left, right) > 0 ){
+                        list[i] = list[j];
+                        list[j] = left;
+                    }
+                }
+        }
+        print("Products sorted by value [ cost * units ]", function(){
+            var productComparerByValue = function(p1, p2){
+                var p1Value = p1.cost * p1.units,
+                    p2Value = p2.cost * p2.units;
+                if (p1Value < p2Value) return -1;
+                if (p1Value === p2Value) return 0;
+                return 1;
+            }
+            sort(products, productComparerByValue);
+            console.table(products);
+        });
+
+    });
 });
+
+//filter
+
+//every
+
+//some
+
+//min
+
+//max
+
+//sum
+
+//aggregate
+
+//groupBy
+
+
 
